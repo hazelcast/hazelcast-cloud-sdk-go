582050b7adbcb6311d10588c770eada80c63ff0a
Dmytro Nosan <dimanosan@gmail.com>
2022-05-17 13:21:09 +0300

    HZC-2870: Add custom classes operations for enterprise clusters

--- enterprise_cluster.go
+++ enterprise_cluster.go
@@ -11,6 +11,14 @@ type EnterpriseClusterService interface {
 	Create(ctx context.Context, input *models.CreateEnterpriseClusterInput) (*models.Cluster, *Response, error)
 	List(ctx context.Context) (*[]models.Cluster, *Response, error)
 	Delete(ctx context.Context, input *models.ClusterDeleteInput) (*models.ClusterId, *Response, error)
+	ListUploadedArtifacts(ctx context.Context,
+		request *models.ListUploadedArtifactsInput) (*[]models.UploadedArtifact, *Response, error)
+	UploadArtifact(ctx context.Context,
+		request *models.UploadArtifactInput) (*models.UploadedArtifact, *Response, error)
+	DeleteArtifact(ctx context.Context,
+		request *models.DeleteArtifactInput) (*models.UploadedArtifact, *Response, error)
+	DownloadArtifact(ctx context.Context,
+		request *models.DownloadArtifactInput) (*models.UploadedArtifactLink, *Response, error)
 }
 
 type enterpriseClusterServiceOp struct {
@@ -22,7 +30,8 @@ func NewEnterpriseClusterService(client *Client) EnterpriseClusterService {
 }
 
 //This function returns detailed configuration of the cluster
-func (c enterpriseClusterServiceOp) Get(ctx context.Context, input *models.GetEnterpriseClusterInput) (*models.Cluster, *Response, error) {
+func (svc enterpriseClusterServiceOp) Get(ctx context.Context,
+	input *models.GetEnterpriseClusterInput) (*models.Cluster, *Response, error) {
 	var cluster models.Cluster
 	var graphqlRequest = models.GraphqlRequest{
 		Name:      "cluster",
@@ -31,12 +40,12 @@ func (c enterpriseClusterServiceOp) Get(ctx context.Context, input *models.GetEn
 		Args:      *input,
 		Response:  cluster,
 	}
-	req, err := c.client.NewRequest(&graphqlRequest)
+	req, err := svc.client.NewRequest(&graphqlRequest)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	resp, err := c.client.Do(ctx, req, &cluster)
+	resp, err := svc.client.Do(ctx, req, &cluster)
 	if err != nil {
 		return nil, resp, err
 	}
@@ -45,7 +54,8 @@ func (c enterpriseClusterServiceOp) Get(ctx context.Context, input *models.GetEn
 }
 
 //This function creates an Enterprise Cluster with a configuration provided in the input
-func (c enterpriseClusterServiceOp) Create(ctx context.Context, input *models.CreateEnterpriseClusterInput) (*models.Cluster, *Response, error) {
+func (svc enterpriseClusterServiceOp) Create(ctx context.Context,
+	input *models.CreateEnterpriseClusterInput) (*models.Cluster, *Response, error) {
 	var cluster models.Cluster
 	var graphqlRequest = models.GraphqlRequest{
 		Name:      "createEnterpriseCluster",
@@ -54,12 +64,12 @@ func (c enterpriseClusterServiceOp) Create(ctx context.Context, input *models.Cr
 		Args:      nil,
 		Response:  cluster,
 	}
-	req, err := c.client.NewRequest(&graphqlRequest)
+	req, err := svc.client.NewRequest(&graphqlRequest)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	resp, err := c.client.Do(ctx, req, &cluster)
+	resp, err := svc.client.Do(ctx, req, &cluster)
 	if err != nil {
 		return nil, resp, err
 	}
@@ -68,7 +78,7 @@ func (c enterpriseClusterServiceOp) Create(ctx context.Context, input *models.Cr
 }
 
 //This function lists all non-deleted Enterprise Clusters
-func (c enterpriseClusterServiceOp) List(ctx context.Context) (*[]models.Cluster, *Response, error) {
+func (svc enterpriseClusterServiceOp) List(ctx context.Context) (*[]models.Cluster, *Response, error) {
 	var clusterList []models.Cluster
 	graphqlRequest := models.GraphqlRequest{
 		Name:      "clusters",
@@ -79,12 +89,12 @@ func (c enterpriseClusterServiceOp) List(ctx context.Context) (*[]models.Cluster
 		},
 		Response: clusterList,
 	}
-	req, err := c.client.NewRequest(&graphqlRequest)
+	req, err := svc.client.NewRequest(&graphqlRequest)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	resp, err := c.client.Do(ctx, req, &clusterList)
+	resp, err := svc.client.Do(ctx, req, &clusterList)
 	if err != nil {
 		return nil, resp, err
 	}
@@ -93,7 +103,8 @@ func (c enterpriseClusterServiceOp) List(ctx context.Context) (*[]models.Cluster
 }
 
 //This function deletes an Enterprise Cluster
-func (c enterpriseClusterServiceOp) Delete(ctx context.Context, input *models.ClusterDeleteInput) (*models.ClusterId, *Response, error) {
+func (svc enterpriseClusterServiceOp) Delete(ctx context.Context,
+	input *models.ClusterDeleteInput) (*models.ClusterId, *Response, error) {
 	var clusterId models.ClusterId
 	graphqlRequest := models.GraphqlRequest{
 		Name:      "deleteCluster",
@@ -102,15 +113,112 @@ func (c enterpriseClusterServiceOp) Delete(ctx context.Context, input *models.Cl
 		Args:      *input,
 		Response:  clusterId,
 	}
-	req, err := c.client.NewRequest(&graphqlRequest)
+	req, err := svc.client.NewRequest(&graphqlRequest)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	resp, err := c.client.Do(ctx, req, &clusterId)
+	resp, err := svc.client.Do(ctx, req, &clusterId)
 	if err != nil {
 		return nil, resp, err
 	}
 
 	return &clusterId, resp, err
+}
+
+func (svc enterpriseClusterServiceOp) ListUploadedArtifacts(ctx context.Context,
+	request *models.ListUploadedArtifactsInput) (*[]models.UploadedArtifact, *Response, error) {
+	var artifact []models.UploadedArtifact
+	graphqlRequest := models.GraphqlRequest{
+		Name:      "customClasses",
+		Operation: models.Query,
+		Input:     nil,
+		Args:      *request,
+		Response:  artifact,
+	}
+	req, err := svc.client.NewRequest(&graphqlRequest)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := svc.client.Do(ctx, req, &artifact)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &artifact, nil, nil
+}
+
+func (svc enterpriseClusterServiceOp) UploadArtifact(ctx context.Context,
+	request *models.UploadArtifactInput) (*models.UploadedArtifact, *Response, error) {
+	var artifact models.UploadedArtifact
+	graphqlQuery := models.GraphqlRequest{
+		Name:      "uploadCustomClassArtifact",
+		Operation: models.Mutation,
+		Input:     nil,
+		Args: models.UploadArtifactArgs{
+			ClusterId: request.ClusterId,
+		},
+		Response: artifact,
+		UploadFile: models.UploadFile{
+			FileName: request.FileName,
+			Content:  request.Content,
+		},
+	}
+	req, err := svc.client.NewUploadFileRequest(&graphqlQuery)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := svc.client.Do(ctx, req, &artifact)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &artifact, nil, nil
+}
+
+func (svc enterpriseClusterServiceOp) DeleteArtifact(ctx context.Context,
+	request *models.DeleteArtifactInput) (*models.UploadedArtifact, *Response, error) {
+	var artifact models.UploadedArtifact
+	graphqlQuery := models.GraphqlRequest{
+		Name:      "deleteCustomClassArtifact",
+		Operation: models.Mutation,
+		Input:     nil,
+		Args:      *request,
+		Response:  artifact,
+	}
+	req, err := svc.client.NewRequest(&graphqlQuery)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := svc.client.Do(ctx, req, &artifact)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &artifact, nil, nil
+}
+
+func (svc enterpriseClusterServiceOp) DownloadArtifact(ctx context.Context,
+	request *models.DownloadArtifactInput) (*models.UploadedArtifactLink, *Response, error) {
+	var artifact models.UploadedArtifactLink
+	graphqlQuery := models.GraphqlRequest{
+		Name:      "downloadCustomClassesArtifact",
+		Operation: models.Query,
+		Args:      *request,
+		Response:  artifact,
+	}
+	req, err := svc.client.NewRequest(&graphqlQuery)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := svc.client.Do(ctx, req, &artifact)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &artifact, nil, nil
 }
